f2fddb02464bab267c4fd32b9b013f02e638ad36
Franny-Alexandro <131605126+Franny-Alexandro@users.noreply.github.com>
2023-08-18 15:42:46 -0400

    Add files via upload

---
+++ libreria/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Librería Project</title>
+    <link rel="stylesheet" href="Style/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Librería </h1>
+        <nav>
+            <ul>
+                <li><a class="btn" href="index.php">Pagina inicio</a></li>
+                <li><a class="btn" href="libros.php">Libros</a></li>
+                <li><a class="btn" href="autores.php">Autores</a></li>
+                <li><a class="btn" href="contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <section class="image-box">
+            <img src="img/Bienvenido.jpg" alt="Imagen de inicio">
+        </section>
+
+        
+
+    </main>
+    
+    <script src="Script.js"></script>
+</body>
+</html>
